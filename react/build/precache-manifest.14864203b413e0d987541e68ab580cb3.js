a12fa25f99eeb27b34c43c76572abd01084cb2cc
lukeren314 <lukeren314@gmail.com>
2020-07-07 14:22:51 -0700

    added build

---
+++ react/build/precache-manifest.14864203b413e0d987541e68ab580cb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f2444ad746c24d94716800335e8c54c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7b38c554b3a7af041b3",
+    "url": "/static/css/main.74647d85.chunk.css"
+  },
+  {
+    "revision": "70916be63063b671c578",
+    "url": "/static/js/2.080898dc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.080898dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7b38c554b3a7af041b3",
+    "url": "/static/js/main.57af5f0b.chunk.js"
+  },
+  {
+    "revision": "3c27d8c784c882ef6f0c",
+    "url": "/static/js/runtime-main.acb8687d.js"
+  }
+]);
