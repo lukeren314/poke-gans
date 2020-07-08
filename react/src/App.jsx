65cbd3d83f079cb2b13f4d8a79304c9a0fd29c3d
lukeren314 <lukeren314@gmail.com>
2020-07-07 17:38:18 -0700

    removed first part of img src for loading img

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -186,11 +186,7 @@ export default class App extends React.Component {
         ) : null}
         {this.state.loading ? (
           <div className="loading">
-            <img
-              src={process.env.PUBLIC_URL + "/loading.png"}
-              className="loading-image"
-              alt="loading"
-            />
+            <img src={"/loading.png"} className="loading-image" alt="loading" />
           </div>
         ) : null}
         {this.state.selectedMonsters &&
