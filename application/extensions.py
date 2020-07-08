6ddd935e3bbcc87324947daac719f86e9ee7f1f6
lukeren314 <lukeren314@gmail.com>
2020-07-07 17:05:48 -0700

    moved constants into functions

--- application/extensions.py
+++ application/extensions.py
@@ -7,19 +7,20 @@ RNN_CHECKPOINT_PATH = './application/checkpoints2'
 RNN_VOCAB_PATH = './application/vocab.json'
 
         
-DCGAN_SAMPLER = dcgan.Sampler(DCGAN_CHECKPOINT_PATH)
-RNN_SAMPLER = rnn.Sampler(
-    RNN_CHECKPOINT_PATH, json.loads(open(RNN_VOCAB_PATH).read()))
+# DCGAN_SAMPLER = 
+# RNN_SAMPLER = 
 
 
 db = SQLAlchemy()
 
 def create_name():
-    return RNN_SAMPLER.sample().split()[0]
+    
+    return rnn.Sampler(
+    RNN_CHECKPOINT_PATH, json.loads(open(RNN_VOCAB_PATH).read())).sample().split()[0]
 
 
 def create_image():
-    image = DCGAN_SAMPLER.sample()
+    image = dcgan.Sampler(DCGAN_CHECKPOINT_PATH).sample()
     image_bytes_array = io.BytesIO()
     image.save(image_bytes_array, format='PNG')
     image_bytes = image_bytes_array.getvalue()
